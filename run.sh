d73551e3fabccf7b52115c1212769f29517471b6
Alfonso Sargiotto <a.sargiotto@itecriocuarto.org.ar>
2023-09-28 20:27:58 -0300

    add comments

--- run.sh
+++ run.sh
@@ -10,4 +10,8 @@ flask db migrate -m "initial migration"
 flask db upgrade
 
 # Start your Flask application
-gunicorn app:app --bind 0.0.0.0:5005
+gunicorn app:app --bind 0.0.0.0:5005 #module_name:application_variable_name
+
+#0.0.0.0: This is the host IP address.
+#Using 0.0.0.0 means Gunicorn will listen on all available network interfaces,
+# making the application accessible from outside the container.
